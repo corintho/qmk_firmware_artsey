920c982219f332d9c679eb1716b3362f11d6ca94
Corintho Assunção <github@corintho.eu>
2025-12-26 17:17:15 +0100

    feat: enable base artsey support

---
+++ keyboards/artsey_io/config.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 Corintho Assuncao
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#include "config_common.h"
+
+/* Board layout */
+#define MATRIX_ROWS 2
+#define MATRIX_COLS 4
+
