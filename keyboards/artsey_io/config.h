3cb75d381f020b1daa8aeee4831bc60c0d7f3111
Corintho Assunção <github@corintho.eu>
2025-12-26 17:17:15 +0100

    feat: enable base artsey support
    fix: remove non-existing header from template
    
    fix: add proper matrix pin setup

---
+++ keyboards/artsey_io/config.h
@@ -0,0 +1,8 @@
+// Copyright (c) 2021 Corintho Assuncao
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+/* Board layout */
+#define MATRIX_ROWS 2
+#define MATRIX_COLS 4
+
---
+++ keyboards/artsey_io/rules.mk
@@ -0,0 +1,5 @@
+BOOTLOADER = rp2040
+CONSOLE_ENABLE = no
+# COMBO_ENABLE = yes # enabled on keymap level
+# MOUSEKEY_ENABLE = yes # already enabled in keyboard.json
+# EXTRAKEY_ENABLE = yes # already enabled in keyboard.json
